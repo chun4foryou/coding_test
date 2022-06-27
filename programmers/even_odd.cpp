6a9a5478816d67360eb5b4d4a77fcfb1dd6fe839
doitnowman <doitnowman@gmail.com>
2022-06-27 22:48:39 +0900

    세번째 Commit

--- programmers/even_odd.cpp
+++ programmers/even_odd.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 
 std::string solution(int num) {
-    string answer = "";
+    std::string answer = "";
     if (num == 0) {
         answer = "Even";
     } else if (num %2 == 0) {
@@ -10,10 +10,12 @@ std::string solution(int num) {
     } else {
         answer = "Odd";
     }
+    return answer;
 }
 
 int main() 
 {
+    solution(10);
 
     return 0;
 }

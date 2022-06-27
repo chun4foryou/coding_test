2f8c754b4aa995f33f77aaf380c2b57c15c0e74e
doitnowman <doitnowman@gmail.com>
2022-06-27 22:46:25 +0900

    두번재 Commit

--- programmers/even_odd.cpp
+++ programmers/even_odd.cpp
@@ -1,8 +1,20 @@
 #include<string>
 #include<vector>
 
+std::string solution(int num) {
+    string answer = "";
+    if (num == 0) {
+        answer = "Even";
+    } else if (num %2 == 0) {
+        answer = "Even";
+    } else {
+        answer = "Odd";
+    }
+}
+
 int main() 
 {
+
     return 0;
 }
 
